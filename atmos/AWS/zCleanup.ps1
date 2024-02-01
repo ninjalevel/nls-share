2435a8f9b81fe5bf4fbb2f94f896e50edcf10112
ninjalevel <dave@ninjalevel.io>
2024-02-01 08:53:52 -0700

    atmos terraform plan vpc-flow-logs-bucket -s org1-plat-ue2-prod worked

--- atmos/AWS/zCleanup.ps1
+++
@@ -1,9 +0,0 @@
-$Paths = [pscustomobject]@{
-    TerraformDir = "C:\DHE\git\nls-infra\root_git\atmos\AWS\components\terraform"
-    LogsDir      = "C:\DHE\git\nls-infra\root_git\atmos\AWS\logs"
-}
-$currentBranch = git rev-parse --abbrev-ref HEAD
-if ($currentBranch -ne 'main') {
-    Remove-Item -Path $Paths.TerraformDir, $Paths.LogsDir -Recurse -Force
-}
-
